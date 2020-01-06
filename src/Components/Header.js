cc8dd0473e19b5028a6aed1c9e11f2a3582ef372
jagritikamboj <59564359+jagritikamboj@users.noreply.github.com>
2020-01-06 17:51:29 +0530

    Header added

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default props => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>b About</li>
+      </ul>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Header from "./Components/Header";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello Jagriti!</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
